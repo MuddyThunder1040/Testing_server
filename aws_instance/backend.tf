6a374d949fb4fa3e2728fd5a186fd844bccd777d
Vishnu3259 <vishnupardha3259@gmail.com>
2025-05-19 22:50:14 -0700

    Added backend

--- aws_instance/backend.tf
+++ aws_instance/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "randbuck"
+    bucket         = "gk6v3te3d4"
     key            = "terraform/state/terraform.tfstate"
     region         = "us-east-2"
     encrypt        = true
