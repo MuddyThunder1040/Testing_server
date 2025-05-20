f6bd028c7e3df955c571a3beae8ceb61845b345b
Vishnu3259 <vishnupardha3259@gmail.com>
2025-05-19 22:48:34 -0700

    updated key

--- aws_instance/backend.tf
+++ aws_instance/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "randbuck"
-    key            = "/home/vishnu/workspace/Testing_Server/terraform.tfstate"
+    key            = "terraform/state/terraform.tfstate"
     region         = "us-east-2"
     encrypt        = true
     dynamodb_table = "tf-locks"
