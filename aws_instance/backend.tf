d03fea52d79f4f549590908eb400c6a86eb240cb
Vishnu3259 <vishnupardha3259@gmail.com>
2025-05-20 15:25:52 -0700

    Removed Backend

--- aws_instance/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "gk6v3te3d4"
-    key            = "terraform/state/terraform.tfstate"
-    region         = "us-east-2"
-    encrypt        = true
-    dynamodb_table = "tf-locks"
-  }
-}
